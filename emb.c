b9ec68410f21a539996bd5bc65f5242e83e53304
RithikaGovindann <rithika@e-consystems.com>
2024-09-14 20:38:49 +0530

    changes added

--- emb.c
+++ emb.c
@@ -2,5 +2,5 @@
 
 int main()
 {
-	printf("I was embarrassed during my training period due to lack of understanding and knowledge..but I never gonna give up");
+	printf("I was embarrassed during my training period due to lack of understanding and knowledge..but I never gonna give up...added changes");
 }
